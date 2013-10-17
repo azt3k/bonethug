7bc5c54bfbdd0f614a60a4ccf92fbabfe9364be6
azt3k <breaks.nz@gmail.com>
2013-10-17 21:55:52 +1300

    commit to build for v 0.0.63

--- lib/bonethug/version.rb
+++ lib/bonethug/version.rb
@@ -1,6 +1,6 @@
 
         module Bonethug
-          VERSION = "0.0.62"
-          BUILD_DATE = "2013-10-17 20:20:15 +1300"
+          VERSION = "0.0.63"
+          BUILD_DATE = "2013-10-17 21:55:52 +1300"
         end
       
