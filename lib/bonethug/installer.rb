36ec5ee4fb90e6f734c2eeb80482dba05aa7f550
azt3k <breaks.nz@gmail.com>
2013-10-23 17:22:40 +1300

    functional composer

--- lib/bonethug/installer.rb
+++ lib/bonethug/installer.rb
@@ -135,13 +135,14 @@ module Bonethug
 
     def self.init_mysql_db_script(db, path, admin_user = 'root')
 
-      # CREATE USER IF NOT EXISTS " + db.get('user') + "@" + db.get('name') + (db.get('pass') ? " IDENTIFIED BY " + db.get('pass') : "") + ";
       script_content = "
         CREATE DATABASE IF NOT EXISTS " + db.get('name') + ";
         GRANT ALL ON " + db.get('name') + ".* TO " + db.get('user') + "@" + db.get('host') + (db.get('pass') ? " IDENTIFIED BY " + db.get('pass') : "") + ";     
         FLUSH PRIVILEGES;
       "
-      cmd = 'cd ' + path + ' && echo "' + script_content + '" > .bonethug/sql.txt && mysql -h ' + db.get('host') + ' -u ' + admin_user + ' -p < .bonethug/sql.txt'
+      cmd = 'cd ' + path + ' && ' +
+            'echo "' + script_content + '" > .bonethug/sql.txt && ' +
+            'mysql -h ' + db.get('host') + ' -u ' + admin_user + ' -p < .bonethug/sql.txt'
 
     end
 
