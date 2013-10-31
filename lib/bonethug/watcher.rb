09a4ee4fe4fa32ff6fe08dc60f69c2c9a6144605
azt3k <breaks.nz@gmail.com>
2013-10-31 16:19:07 +1300

    slim parser

--- lib/bonethug/watcher.rb
+++ lib/bonethug/watcher.rb
@@ -57,14 +57,14 @@ module Bonethug
       erb = []
       if erbs = conf.get('watch.erb')
         erbs.each do |index, watch|
-          erb.push(src: watch.get('src','Array'), dest: watch.get('dest'), filter: watch.get('filter'), type: :erb)
+          erb.push(src: watch.get('src'), dest: watch.get('dest'), filter: watch.get('filter'), type: :erb)
         end
       end
 
       slim = []
       if erbs = conf.get('watch.slim')
         slims.each do |index, watch|
-          slim.push(src: watch.get('src','Array'), dest: watch.get('dest'), filter: watch.get('filter'), type: :slim)
+          slim.push(src: watch.get('src'), dest: watch.get('dest'), filter: watch.get('filter'), type: :slim)
         end
       end  
 
