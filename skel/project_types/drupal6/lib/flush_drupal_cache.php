78befe30a8224b62b7fa2557171230dc8e3ad7b9
azt3k <breaks.nz@gmail.com>
2013-11-27 09:32:57 +1300

    bug fixes

--- skel/project_types/drupal6/lib/flush_drupal_cache.php
+++ skel/project_types/drupal6/lib/flush_drupal_cache.php
@@ -1,7 +1,7 @@
 <?php
-$pub_dir = __DIR__ . '/../public';
+$pub_dir = realpath(__DIR__ . '/../public');
 chdir($pub_dir);
 define('DRUPAL_ROOT', $pub_dir);
-require_once 'includes/bootstrap.inc';
+require_once $pub_dir . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 drupal_flush_all_caches();
--- skel/project_types/drupal8/lib/flush_drupal_cache.php
+++ skel/project_types/drupal8/lib/flush_drupal_cache.php
@@ -1,5 +1,7 @@
 <?php
-chdir(__DIR__ . '/../public');
-require_once 'includes/bootstrap.inc';
+$pub_dir = realpath(__DIR__ . '/../public');
+chdir($pub_dir);
+define('DRUPAL_ROOT', $pub_dir);
+require_once $pub_dir . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 drupal_flush_all_caches();
