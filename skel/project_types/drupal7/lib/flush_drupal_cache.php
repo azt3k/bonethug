0a8656fae0b214162363549f7439398ababc6874
azt3k <breaks.nz@gmail.com>
2013-11-27 10:15:33 +1300

    bug fixes

--- skel/project_types/drupal7/lib/flush_drupal_cache.php
+++ skel/project_types/drupal7/lib/flush_drupal_cache.php
@@ -1,7 +1,11 @@
 <?php
+
 $pub_dir = realpath(__DIR__ . '/../public');
 chdir($pub_dir);
 define('DRUPAL_ROOT', $pub_dir);
-require_once $pub_dir . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-drupal_flush_all_caches();
+
+if (file_exists($pub_dir . '/includes/bootstrap.inc')) {
+    require_once $pub_dir . '/includes/bootstrap.inc';
+    drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+    drupal_flush_all_caches();
+}
