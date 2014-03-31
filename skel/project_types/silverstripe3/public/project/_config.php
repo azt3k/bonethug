c40218ca434e8c77c3130e60df78ce89833fb0e2
Aaron Latham-Ilari <breaks.nz@gmail.com>
2014-04-01 10:19:40 +1300

    updated managed file index for ss projects

--- skel/project_types/silverstripe3/public/project/_config.php
+++ skel/project_types/silverstripe3/public/project/_config.php
@@ -25,42 +25,42 @@ switch(APPLICATION_ENV){
 
 		// Debug Settings
 		ini_set("display_errors",1);
-		error_reporting(E_ALL & ~E_STRICT);		
+		error_reporting(E_ALL & ~E_STRICT);
 		Director::set_environment_type("dev");
-		
+
 		// Ensure that all emails get directed to the developer email address
 		Config::inst()->update('Email', 'send_all_emails_to', SS_SEND_ALL_EMAILS_TO);
-		
+
 		// Set admin email
 		Config::inst()->update('Email', 'admin_email', SS_SEND_ALL_EMAILS_TO);
-		
+
 		// Log file
 		SS_Log::add_writer(new SS_LogFileWriter(BASE_PATH.'/../log/development.log'), SS_Log::WARN, '<=');
 
 		// hard code user / pass
 		Security::setDefaultAdmin('admin', 'admin');
-		
+
 		break;
 
 	case 'staging':	
 
 		// Debug Settings
 		ini_set("display_errors",1);
-		error_reporting(E_ALL & ~E_STRICT);		
+		error_reporting(E_ALL & ~E_STRICT);
 		Director::set_environment_type("test");
-		
+
 		// Ensure that all emails get directed to the developer email address
 		Email::send_all_emails_to(SS_SEND_ALL_EMAILS_TO);
-		
+
 		// Set admin email
 		Email::setAdminEmail(SS_SEND_ALL_EMAILS_TO);
 
 		// hard code user / pass
-		Security::setDefaultAdmin('admin', 'admin');			
-		
+		Security::setDefaultAdmin('admin', 'admin');
+
 		// Log file
 		SS_Log::add_writer(new SS_LogFileWriter(BASE_PATH.'/../log/staging.log'), SS_Log::WARN, '<=');
-		
+
 		break;
 
 	default:
@@ -72,7 +72,7 @@ switch(APPLICATION_ENV){
 		SS_Log::add_writer(new SS_LogFileWriter(BASE_PATH.'/../log/production.log'), SS_Log::WARN, '<=');
 
 		break;
-		
+
 }
 
 
@@ -96,7 +96,7 @@ $databaseConfig = array(
 	"password" 	=> $ss_cnf->db->pass,
 	"database" 	=> $ss_cnf->db->name,
 	"path" 		=> '',
-);	
+);
 Config::inst()->update('MySQLDatabase', 'connection_charset', 'utf8');
 
 
@@ -143,7 +143,7 @@ Requirements::set_combined_files_enabled(true);
 $shimRequirements = array(
     'vendor/selectivizr/selectivizr.min.js',
     'vendor/respond/respond.min.js',
-    'vendor/modernizr/modernizr.js'      
+    'vendor/modernizr/modernizr.js'
 );
 Requirements::combine_files('lte-ie8-shims.js',$shimRequirements);
 Requirements::process_combined_files(); // forces ss to generate the file regardless of blocking
@@ -153,7 +153,7 @@ Requirements::process_combined_files(); // forces ss to generate the file regard
 
 $cssRequirements = array(
     'themes/project/css/main.css',
-    'themes/project/css/typography.css'   
+    'themes/project/css/typography.css'
 );
 Requirements::combine_files('application.css', $cssRequirements);
 
@@ -184,7 +184,7 @@ RequirementsHelper::require_block(array_merge(
 Requirements::insertHeadTags('
     <!--[if (gte IE 6)&(lte IE 8)]>
         <script src="/assets/_combinedfiles/lte-ie8-shims.js"></script>
-    <![endif]--> 
+    <![endif]-->
 ');
 
 // block all front end requirements from the cms
@@ -193,7 +193,7 @@ LeftAndMainHelper::require_block(array_merge(
         'assets/_combinedfiles/application.css',
         'assets/_combinedfiles/application.js',
         'assets/_combinedfiles/lte-ie8-shims.js'
-    ),   
+    ),
     $cssRequirements,
     $jsRequirements,
     $shimRequirements
