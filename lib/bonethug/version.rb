f785c5467c489f0ae307e017504de5fc7e687eeb
Aaron Latham-Ilari <az@dev-io.us>
2015-08-10 08:29:38 +1200

    commit to build for v 0.0.97

--- lib/bonethug/version.rb
+++ lib/bonethug/version.rb
@@ -1,6 +1,6 @@
 
         module Bonethug
-          VERSION = "0.0.96"
-          BUILD_DATE = "2015-05-09 07:23:35 +1200"
+          VERSION = "0.0.97"
+          BUILD_DATE = "2015-08-10 08:29:38 +1200"
         end
       
